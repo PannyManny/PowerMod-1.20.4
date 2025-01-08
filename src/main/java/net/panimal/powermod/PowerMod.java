b421ad73ed3dda59e23307173b50c70248d8de9b
matthew.mcpherson08@gmail.com <matthew.mcpherson08@gmail.com>
2025-01-08 12:28:06 -0600

    Initial commit

---
+++ src/main/java/net/panimal/powermod/PowerMod.java
@@ -0,0 +1,15 @@
+package net.panimal.powermod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class PowerMod implements ModInitializer {
+	public static final String MOD_ID = "powermod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/net/panimal/powermod/PowerModClient.java
@@ -0,0 +1,10 @@
+package net.panimal.powermod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PowerModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
